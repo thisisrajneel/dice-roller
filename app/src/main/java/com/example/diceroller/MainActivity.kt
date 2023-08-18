ea0bfe6142e59795c26ab6dd1d9e294a0e98f7b7
Rajneel Anuraj <rajneelanuraj2001@gmail.com>
2023-08-18 15:39:57 +0530

    Dice Roller done

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -3,9 +3,11 @@ package com.example.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.core.math.MathUtils
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,8 +21,17 @@ class MainActivity : AppCompatActivity() {
     private fun rollDice() {
         // Can't pass Views or anything by ref in kt
         val txt: TextView = findViewById<TextView>(R.id.txtNumber)
+        val img: ImageView = findViewById<ImageView>(R.id.diceImg)
         txt.text = (Math.random()*6 + 1).toInt().toString()
-        Toast.makeText(this, "Updated text to ${txt.text.toString()}", Toast.LENGTH_SHORT).show()
+        when (txt.text) {
+            "1" -> {img.setImageResource(R.drawable.dice_1)}
+            "2" -> {img.setImageResource(R.drawable.dice_2)}
+            "3" -> {img.setImageResource(R.drawable.dice_3)}
+            "4" -> {img.setImageResource(R.drawable.dice_4)}
+            "5" -> {img.setImageResource(R.drawable.dice_5)}
+            "6" -> {img.setImageResource(R.drawable.dice_6)}
+        }
+        Toast.makeText(this, "Updated text to ${txt.text}", Toast.LENGTH_SHORT).show()
 
     }
 }
